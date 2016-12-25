05b78a47fcb8739bcf26480a6165ac976ab33d98
Jonathan Lima <greenboxal@gmail.com>
2016-12-25 12:26:26 -0200

    Rename StandardHandler to MuxHandler

--- muxhandler.go
+++ muxhandler.go
@@ -1,29 +1,29 @@
 package routedrpc
 
-// StandardHandler multiplexes calls to other handlers based on the target address
-type StandardHandler struct {
+// MuxHandler multiplexes calls to other handlers based on the target address
+type MuxHandler struct {
 	handlers map[Address]Handler
 }
 
-// NewStandardHandler creates a new _StandardHandler_
-func NewStandardHandler() *StandardHandler {
-	return &StandardHandler{
+// NewMuxHandler creates a new _MuxHandler_
+func NewMuxHandler() *MuxHandler {
+	return &MuxHandler{
 		handlers: make(map[Address]Handler),
 	}
 }
 
 // Register a handler for an address
-func (n *StandardHandler) Register(address Address, handler Handler) {
+func (n *MuxHandler) Register(address Address, handler Handler) {
 	n.handlers[address] = handler
 }
 
 // Deregister a handler
-func (n *StandardHandler) Deregister(address Address) {
+func (n *MuxHandler) Deregister(address Address) {
 	delete(n.handlers, address)
 }
 
 // HasTarget returns if this target address can be processed in this node
-func (n *StandardHandler) HasTarget(target Address) (bool, bool) {
+func (n *MuxHandler) HasTarget(target Address) (bool, bool) {
 	handler, found := n.handlers[target]
 
 	if !found {
@@ -34,7 +34,7 @@ func (n *StandardHandler) HasTarget(target Address) (bool, bool) {
 }
 
 // HandleCast forwards the call to the correct handler based on the target address
-func (n *StandardHandler) HandleCast(sender, target Address, message interface{}) error {
+func (n *MuxHandler) HandleCast(sender, target Address, message interface{}) error {
 	handler, found := n.handlers[target]
 
 	if !found {
@@ -45,7 +45,7 @@ func (n *StandardHandler) HandleCast(sender, target Address, message interface{}
 }
 
 // HandleCall forwards the call to the correct handler based on the target address
-func (n *StandardHandler) HandleCall(sender, target Address, message interface{}) (interface{}, error) {
+func (n *MuxHandler) HandleCall(sender, target Address, message interface{}) (interface{}, error) {
 	handler, found := n.handlers[target]
 
 	if !found {
